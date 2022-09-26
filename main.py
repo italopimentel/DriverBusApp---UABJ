1887545ab0a11996df6a4da9b93e906763c3cd6a
italopimentel <italo.pimentel@ufrpe.br>
2022-09-26 16:58:14 -0300

    Version 0.4

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from kivy.utils import platform
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.clock import Clock
 from lib.filemanagment import FileManagment
+import requests
+from plyer import gps
 
-userPosition = {'lat': -8.332996, 'lon': -36.416745}
+userPosition = {'id': 0, 'lat': 0, 'lon': 0}
 
 class ScreenManagment(ScreenManager):
     pass
-
 
 class LoginScreen(Screen):
     login = "italoph"
@@ -24,30 +25,31 @@ class LoginScreen(Screen):
             self.fileManagment.writeUserInfo(self.ids.loginText.text, self.ids.passwordText.text)
             self.manager.current = "mainscreen"
 
-
 class MainScreen(Screen):
+
+    activateGps = False
+    markerList = [MapMarker(), MapMarker()]
 
     def drawOnMap(self):
         pass
 
-    def setStartPosition(self, dt):
-        global userPosition
-        self.ids.map.center_on(userPosition['lat'], userPosition['lon'])
-        self.ids.localBus.lat = userPosition['lat']
-        self.ids.localBus.lon = userPosition['lon']
-        self.ids.map.zoom = 15
-
     def updatePosition(self, dt):
         global userPosition
         if platform == 'android':
-            self.ids.localBus.lat = userPosition['lat']
-            self.ids.localBus.lon = userPosition['lon']
-        else:
-            self.ids.localBus.lat = userPosition['lat']
-            self.ids.localBus.lon = userPosition['lat']
+            self.markerList[0] = self.markerList[1]
+            self.markerList[1] = self.addMarker(userPosition['lat'], userPosition['lon'], source="imgs/onibus_local.png")
+            self.ids.map.center_on(userPosition['lat'], userPosition['lon'])
+            self.ids.map.zoom = 18
+            self.ids.map.add_marker(self.markerList[1])
+            self.ids.map.remove_marker(self.markerList[0])
+            print(userPosition)
+            try:
+                requests.post("https://Aplicativo-Onibus.italopimentel.repl.co/routers", json=userPosition)
+            except:
+                print("Não foi possível enviar a rota, ocorreu algum erro")
 
     def addMarker(self, lat, lon, source):
-        self.ids.map.add_widget(MapMarker(lat=lat, lon=lon, source=source))
+        return MapMarker(lat=lat, lon=lon, source=source)
 
     def multipleButtonsCall(self, instance):
         global userPosition
@@ -58,11 +60,17 @@ class MainScreen(Screen):
             self.manager.get_screen("loginscreen").fileManagment.deleteUserInfo()
             self.manager.current = "loginscreen"
 
+    def gpsControler(self):
+        pass
+
+    def startService(self):
+        self.gpsControler()
 
 class MapApp(MDApp):
 
     fileManagment = FileManagment()
     userInfo = fileManagment.getUserInfo()
+    loggedIn = False
 
     def on_loc_update(self, **kwargs):
         global userPosition
@@ -72,9 +80,8 @@ class MapApp(MDApp):
 
     def on_start(self):
         if platform == 'android':
-            from plyer import gps
             gps.configure(on_location=self.on_loc_update)
-            gps.start()
+            gps.start(minTime=3000, minDistance=1)
 
     def build(self):
         self.theme_cls.theme_style = 'Dark'
@@ -82,14 +89,11 @@ class MapApp(MDApp):
         self.title = 'Tela Inicial'
 
         screenManagment = ScreenManagment()
+        Clock.schedule_interval(screenManagment.get_screen("mainscreen").updatePosition, 3)
 
-        if platform == 'android':
-            Clock.schedule_interval(screenManagment.get_screen("mainscreen").updatePosition, 1)
-            Clock.schedule_once(screenManagment.get_screen("mainscreen").setStartPosition)
-        else:
-            Clock.schedule_once(screenManagment.get_screen("mainscreen").setStartPosition)
         try:
             if self.userInfo["login"] == "italoph" and self.userInfo["password"] == "12345":
+                self.loggedIn == True
                 screenManagment.current = "mainscreen"
         except:
             print("Login ou senha vazios ou incorretos!")
@@ -97,4 +101,4 @@ class MapApp(MDApp):
         return screenManagment
 
 
-MapApp().run()
+app = MapApp().run()
